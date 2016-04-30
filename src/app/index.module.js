1c10e733b3610b795dc97dc6dd01cf49c34a29ff
James Lynch <jim@Jamess-MacBook-Pro.local>
2016-04-29 22:49:04 -0700

    first commit

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('hillaryHackathon', ['ngAnimate', 'ngCookies', 'ngSanitize', 'ngMessages', 'ngResource', 'ui.router', 'ui.bootstrap', 'toastr']);
+
+})();
